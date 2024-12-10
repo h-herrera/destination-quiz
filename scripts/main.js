bdb69ad39a2b5304928ff22231d0b1c1dd94a706
Hailey Herrera <h.herrera@ufl.edu>
2024-12-09 19:02:05 -0500

    Final Commit

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,21 @@ let summary = document.querySelectorAll(".summary");
 let grade = document.querySelector("#score");
 let correct = 0;
 let incorrect = 0; 
+let wrong = "This is incorrect.";
+let right = "This is correct.";
+let message = document.querySelectorAll(".message");
+let message1 = document.querySelector("#message1");
+let message2 = document.querySelector("#message2");
+let message3 = document.querySelector("#message3");
+let message4 = document.querySelector("#message4");
+let message5 = document.querySelector("#message5");
+let message6 = document.querySelector("#message6");
+let message7 = document.querySelector("#message7");
+let message8 = document.querySelector("#message8");
+let message9 = document.querySelector("#message9");
+let message10 = document.querySelector("#message10");
+let messasge11 = document.querySelector("#message11");
+let message12 = document.querySelector("#message12");
 let a1 = form.elements["area1"];
 let a2 = form.elements["area2"];
 let a3 = form.elements["area3"]; 
@@ -50,44 +65,94 @@ function showAll() {
     grade.style.display = 'block'
 };
 
+function writeResults () {
+    document.querySelector("#message1").textContent = message1
+    document.querySelector("#message2").textContent = message2
+    document.querySelector("#message3").textContent = message3
+    document.querySelector("#message4").textContent = message4
+    document.querySelector("#message5").textContent = message5
+    document.querySelector("#message6").textContent = message6
+    document.querySelector("#message7").textContent = message7
+    document.querySelector("#message8").textContent = message8
+    document.querySelector("#message9").textContent = message9
+    document.querySelector("#message10").textContent = message10
+    document.querySelector("#message11").textContent = message11
+    document.querySelector("#message12").textContent = message12
+
+};
+
 function checkAnswers() {
     if (a1.value === answers.q1) {
         correct++;
-    }else {
-        incorrect++;
+        message1 = right;
+    }else{
+        message1 = wrong;
     }; 
     if (a2.value === answers.q2) {
         correct++;
+        message2 = right;
+    }else{
+        message2 = wrong;
     }; 
     if (a3.value === answers.q3) {
         correct++;
+        message3 = right;
+    }else{
+        message3 = wrong;
     }; 
     if (a4.value === answers.q4) {
         correct++;
+        message4 = right;
+    }else{
+        message4 = wrong;
     }; 
     if (a5.value === answers.q5) {
         correct++;
+        message5 = right;
+    }else{
+        message5 = wrong;
     }; 
     if (a6.value === answers.q6) {
         correct++;
+        message6 = right;
+    }else{
+        message6 = wrong;
     }; 
     if (a7.value === answers.q7) {
         correct++;
+        message7 = right;
+    }else{
+        message7 = wrong;
     }; 
     if (a8.value === answers.q8) {
         correct++;
+        message8 = right;
+    }else{
+        message8 = wrong;
     }; 
     if (a9.value === answers.q9) {
         correct++;
+        message9 = right;
+    }else{
+        message9 = wrong;
     }; 
     if (a10.value === answers.q10) {
         correct++;
+        message10 = right;
+    }else{
+        message10 = wrong;
     }; 
     if (a11.value === answers.q11) {
         correct++;
+        message11 = right;
+    }else{
+        message11 = wrong;
     }; 
     if (a12.value === answers.q12) {
         correct++;
+        message12 = right;
+    }else{
+        message12 = wrong;
     }; 
 };
 
@@ -99,6 +164,8 @@ form.onsubmit = (e) => {
 showAll();
 
 checkAnswers();
+
+writeResults();
 
 grade.textContent = `Your Score: ${correct} / 12`;
 
